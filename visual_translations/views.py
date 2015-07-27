3c6f3f7c3ccfea97d47a1f59a52278ce2fa69dc9
fako <email@fakoberkers.nl>
2015-07-27 17:29:20 +0200

    Put all languages in one big row.

--- visual_translations/views.py
+++ visual_translations/views.py
@@ -12,6 +12,7 @@ class VisualTranslationsHtmlView(HtmlCommunityView):
             lang_key = lambda obj: obj["language"]
             data["results"].sort(key=lang_key)
             response.data["results"] = [(lang, list(words),) for lang, words in groupby(data["results"], lang_key)]
+            response.data["results"] *= 3
             return response.data
         else:
             return response.data
