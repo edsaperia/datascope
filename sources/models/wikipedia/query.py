4fc2b880df39abf54bc5fb403c93f34a89099d34
Fako Berkers <email@fakoberkers.nl>
2016-12-04 15:00:57 +0100

    Add wikipedia category and category member sources.

--- sources/models/wikipedia/query.py
+++ sources/models/wikipedia/query.py
@@ -24,6 +24,7 @@ class WikipediaQuery(HttpResource):
     CONFIG_NAMESPACE = "wikipedia"
     WIKI_RESULTS_KEY = "pages"
     WIKI_QUERY_PARAM = "titles"
+    ERROR_MESSAGE = "We did not find the page you were looking for. Perhaps you should create it?"
 
     def send(self, method, *args, **kwargs):
         args = (self.config.wiki_country, self.WIKI_QUERY_PARAM,) + args
@@ -42,16 +43,14 @@ class WikipediaQuery(HttpResource):
             raise DSInvalidResource('Wrongly formatted Wikipedia response, missing "query"', resource=self)
         response = data['query'][self.WIKI_RESULTS_KEY]  # Wiki has response hidden under single keyed dicts :(
 
-        # We force a 404 on missing pages
-        message = "We did not find the page you were looking for. Perhaps you should create it?"
         # When searching for pages a dictionary gets returned
         if isinstance(response, dict) and "-1" in response and "missing" in response["-1"]:
             self.status = 404
-            raise DSHttpError40X(message, resource=self)
+            raise DSHttpError40X(self.ERROR_MESSAGE, resource=self)
         # When making lists a list is returned
         elif isinstance(response, list) and not response:
             self.status = 404
-            raise DSHttpError40X(message, resource=self)
+            raise DSHttpError40X(self.ERROR_MESSAGE, resource=self)
 
         return response
 
@@ -68,3 +67,19 @@ class WikipediaQuery(HttpResource):
 
     class Meta:
         abstract = True
+
+
+class WikipediaGenerator(WikipediaQuery):
+
+    def _handle_errors(self):
+        """
+        Generators have a habit of leaving out the query parameter if the query returns nothing :(
+        :return:
+        """
+        try:
+            super(WikipediaGenerator, self)._handle_errors()
+        except DSInvalidResource:
+            # This indicates the generator didn't find anything under the 'query' key in body
+            # In practise it means the searched for title does not exist.
+            self.status = 404
+            raise DSHttpError40X(self.ERROR_MESSAGE, resource=self)
